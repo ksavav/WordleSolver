05d81cad33c345fb345d57d26df2efbb7d6c9bbc
xawer <xawer@DESKTOP-56N85KO>
2023-01-10 00:12:07 +0100

    Dodaj pliki projektów.

---
+++ WordleSolver/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebScrap
+{
+    class Program
+    {
+        static public int _attempt = 1;
+
+        static void Main(string[] args)
+        {
+            var w = new WebScraper();
+
+            Thread.Sleep(10000);
+
+            var xd = w.GetDataFromWeb(_attempt);
+
+            foreach (var item in xd)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
+
+
+/* TODO
+ * przeniesienie webscrapa do oddzielnej klasy DONE
+ * przetwarzanie informacji
+ * dodanie slownika
+ * logika wybierania slowa
+ */
