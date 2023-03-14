43b7653f8412e32c9a253fd02c40ce2951ebe989
Ksawery Armatynski <a.xawery@gmail.com>
2023-03-14 14:08:55 +0100

    fixed word logic

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -1,33 +1,43 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using WordleSolver;
 
 namespace WebScrap
 {
     class Program
     {
         static public int _attempt = 1;
-
+        static bool isOver = false;
+        const string startingWord = "ramen";
+        //tares, crane
         static void Main(string[] args)
         {
             var w = new WebScraper();
 
-            Thread.Sleep(10000);
+            Thread.Sleep(2000);
 
-            var xd = w.GetDataFromWeb(_attempt);
-
-            foreach (var item in xd)
+            var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "words.txt");
+            var words = new List<string>(File.ReadAllLines(path));
+            
+            var upperWords = new List<string>();
+            foreach (var word in words)
             {
-                Console.WriteLine(item);
+                upperWords.Add(word.ToUpper());
+            }
+
+            var xd = new CheckPresents();
+            xd.FirstWord(startingWord.ToUpper());
+            Thread.Sleep(2000);
+
+            while (isOver == false)
+            {
+                var data = w.GetDataFromWeb(_attempt);
+                isOver = xd.CheckPresentsAndPassNewWordToType(data, upperWords);
+
+                _attempt++;
+                Thread.Sleep(2000);
             }
         }
-
     }
 }
-
-
-/* TODO
- * przeniesienie webscrapa do oddzielnej klasy DONE
- * przetwarzanie informacji
- * dodanie slownika
- * logika wybierania slowa
- */
